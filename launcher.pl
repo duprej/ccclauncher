a2f5f4206e3540971a8b4359ecfc55f358bd216b
Jonathan DUPRE <duprejmail-forums@yahoo.fr>
2020-10-03 19:04:49 +0200

    Initial commit

---
+++ launcher.pl
@@ -0,0 +1,378 @@
+=pod
+	CCClauncher Perl script for Linux (init/systemd and others)
+	Developped for Pioneer CAC Autochangers application CCCpivot.
+	A simple Perl script to launch & manage Node.js CCCpivot processes.
+	2018-2020 Jonathan DUPRE <http://www.jonathandupre.fr>
+	Licenced under GPLv3
+	1.0 - sept. 23 2020
+=cut
+use constant SCRIPTVERSION => '1.0';
+use strict;
+use warnings;
+use v5.10;
+use Config::Simple;
+use DBI;
+use Proc::Simple;
+use String::Util qw(trim);
+use Switch;
+use Sys::Hostname;
+use Text::CSV qw(csv);
+
+# ------------------------ GLOBAL VARS ------------------------ 
+my $confFileName="/etc/ccclauncher.cfg";
+my $csvFileName="/etc/cccchangers.csv";
+my $csvPivotScript="/opt/cccpivot/pivot.js";
+my $pidPivotsFile="/var/run/cccpivot.pids";
+my $logDir="/var/log/";
+my $cfgObj;					#Config simple object (from .cfg file)
+my %autochangers;			#Hashtable Datasource (autochangers configuration from CSV or Postgres)
+my %processes;				#Hashtable PID file (processes launched)
+my $ignoredAutochangers=0;	#Count of ignored (no hostname matching) autochangers from Datasource configuration
+my $disabledAutochangers=0;	#Count of disabled autochangers from Datasource configuration
+my $counter = 0;			#Generic counter var used in various places
+
+
+# ------------------------ FUNCTIONS ------------------------ 
+# Print script usage if bad parameters given
+sub printUsage() {
+	print "This script needs a single parameter : [start|restart|status|stop|clean|kill].\n";
+	print "Usage : $0 [start|restart|status|stop|clean|kill].\n";
+}
+
+# Return false or true if a process number exists.
+sub checkPIDExists($) {
+	my ($pid)=@_;
+	return (-e "/proc/$pid/stat");
+}
+
+# Load configuration form local CSV file Datasource
+sub loadCsvDs($) {
+	my $host = shift;
+	my $csvObj = Text::CSV->new();
+	$csvObj->sep(';');
+	# Open, parse and load CSV File in a row
+	my $ref = csv (in => $csvFileName, key => "id", headers => "auto", encoding => "UTF-8", sep_char=> ";");
+	%autochangers = %$ref;
+	# Remove those are not matching the hostname or disabled
+	foreach my $key (keys %autochangers) {
+		my $jukebox = $autochangers{$key};
+		my %jukebox = %$jukebox;
+		if ($jukebox{'hostname'} ne $host) {
+			delete($autochangers{$key});
+			$ignoredAutochangers++; # Counter of ignored lines
+		}
+		if ($jukebox{'enabled'} ne 'true') {
+			delete($autochangers{$key});
+			$disabledAutochangers++; # Counter of disabled lines
+		}
+	}
+}
+
+# Load configuration form Postgres DB Datasource
+sub loadPgDbDs($) {
+	my $return = undef;
+	my $host = shift;
+	my $dsString = "DBI:Pg:dbname=".trim($cfgObj->param('database.name')).';host='.trim($cfgObj->param('database.host')).';port='.trim($cfgObj->param('database.port'));
+	my $dbHandle = DBI->connect($dsString, trim($cfgObj->param('database.user')), trim($cfgObj->param('database.password')),{ RaiseError => 1, AutoCommit => 0 });
+	if (defined $dbHandle) {
+		$dbHandle->do("SET search_path TO ccc, public");
+		my $stHandle = $dbHandle->prepare("SELECT ac.id,description,tcpPort,serialPort,bauds,timeout,password,type,leftplayerid,hostname,enabled,usetls FROM autochanger ac, autochangermodel acm WHERE ac.model = acm.id ORDER BY ac.id;");
+		my $returnValue = $stHandle->execute();
+		if ($returnValue < 0) {
+			print "ERROR : Problem with the SQL query.\n";
+			print "ERROR : ".$DBI::errstr;
+		} else {
+			my $row;
+			while($row = $stHandle->fetchrow_hashref()) {
+				if ($row->{'hostname'} ne $host) {
+					$ignoredAutochangers++; # Counter of ignored lines
+					last;
+				}
+				if ($row->{'enabled'} ne '1') {
+					$disabledAutochangers++; # Counter of disabled lines
+					last;
+				}
+				my %newJB;
+				$newJB{'id'} = 'jb'.$row->{'id'};
+				$newJB{'desc'} = $row->{'description'};
+				$newJB{'tcpPort'} = $row->{'tcpport'};
+				$newJB{'serialPort'} = $row->{'serialport'};
+				$newJB{'bauds'} = $row->{'bauds'};
+				$newJB{'timeout'} = $row->{'timeout'};
+				$newJB{'password'} = $row->{'password'};
+				$newJB{'model'} = $row->{'type'};
+				$newJB{'leftPlayerID'} = $row->{'leftplayerid'};
+				$newJB{'useTLS'} = $row->{'usetls'};
+				$autochangers{$newJB{'id'}} = \%newJB;
+			}
+		}
+		$return = 1;
+		undef $stHandle;
+		$dbHandle->disconnect();
+	} else {
+		print "ERROR : Enable to connect to database.\n";
+		print "ERROR : ".$DBI::errstr;
+	}
+	return $return;
+}
+
+# Load current launched processes from PID file
+sub loadPidFile() {
+	my $csvObj = Text::CSV->new();
+	$csvObj->sep(';');
+	if (!-e $pidPivotsFile || !-f _ || !-r _ ) {
+		print "INFO : No PID file.\n";
+			%processes = ();
+	} else {
+		my $ref = csv (in => $pidPivotsFile, key => "key", headers => "auto", encoding => "UTF-8", sep_char=> ";");
+		if (defined $ref) {
+			%processes = %$ref;
+		}
+	}
+}
+
+sub execStop() {
+	# Get info from PIDs file and stop processes
+	loadPidFile();
+	if (keys %processes == 0) {
+		print "INFO : There is no running process in the PID file. Nothing to stop.\n";
+	} else {
+		# Stopping processes
+		$counter = 0;
+		foreach my $key (sort(keys %processes)) {
+			my $process = $processes{$key};
+			my %process = %$process;
+			my $pid = $process{'pid'};
+			if (!checkPIDExists($pid)) {
+				# Dead process
+				printf("ERROR : CCCpivot script for %s : KO, process n°%s is dead.\n",$key,$pid);
+				$counter++;
+			} else {
+				printf("INFO : Stopping CCCpivot script for %s... : ",$key);
+				# Existing process, let's try to terminate it
+				system("kill -SIGTERM $pid");
+				sleep 1; # Wait a little bit.
+				# Re-check
+				if (checkPIDExists($pid)) {
+					printf("KO, process n°%s can't be stopped. Bad user? Try kill method.\n",$pid);
+				} else {
+					printf("OK, process n°%s as been stopped properly.\n",$pid);
+					$counter++;
+				}
+			}
+		}
+		if ($counter == keys %processes) {
+			# If all processus terminated, remove PID file.
+			system("rm $pidPivotsFile");
+		}
+	}
+}
+
+sub execKill() {
+	# Get info from PIDs file and stop processes
+	loadPidFile();
+	if (keys %processes == 0) {
+		print "INFO : There is no running process in the PID file. Nothing to kill.\n";
+	} else {
+		# Killing processes
+		$counter = 0;
+		foreach my $key (sort(keys %processes)) {
+			my $process = $processes{$key};
+			my %process = %$process;
+			my $pid = $process{'pid'};
+			if (!checkPIDExists($pid)) {
+				# Dead process
+				printf("ERROR : CCCpivot script for %s : KO, process n°%s is dead.\n",$key,$pid);
+				$counter++;
+			} else {
+				printf("INFO : Killing CCCpivot script for %s... : ",$key);
+				# Existing process, let's try to terminate it
+				system("kill -SIGKILL $pid");
+				sleep 1; # Wait a little bit.
+				# Re-check
+				if (checkPIDExists($pid)) {
+					printf("KO, process n°%s can't be killed. Bad user?\n",$pid);
+				} else {
+					printf("OK, process n°%s as been killed properly.\n",$pid);
+					$counter++;
+				}
+			}
+		}
+		if ($counter == keys %processes) {
+			# If all processus terminated, remove PID file.
+			system("rm $pidPivotsFile");
+		}
+	}
+}
+
+sub execStart() {
+	# Determine hostname for filtering
+	my $host=(trim($cfgObj->param("general.hostname"))) ? trim($cfgObj->param("general.hostname")) : hostname();
+	printf "The hostname is %s.\n", $host;
+	# Determine datasource and get data
+	switch(my $ds = trim($cfgObj->param('general.datasource'))) {
+		case "db" {
+			print "Datasource is Postgres database.\n";
+			if (loadPgDbDs($host)) {
+					print "INFO : Postgres datasource OK.\n";
+				} else {
+					print "ERROR : Problem with Postgres datasource.\n";
+					exit 11;
+				}
+		}
+		case "csv" {
+			print "Datasource is CSV file.\n";
+			if (!-e $csvFileName || !-f _ || !-r _ ) {
+				printf "ERROR : Datasource file %s not found or not readable.\n", $csvFileName;
+				exit 2;
+			}
+			loadCsvDs($host);
+		}
+		else {
+			printf "ERROR : Datasource method '%s' is not valid.\n", $ds;
+			exit 5;
+		}
+	}
+	# Check there is something to do
+	printf("INFO : %d autochangers loaded, %d disabled, %d ignored.\n", scalar(keys %autochangers),$disabledAutochangers,$ignoredAutochangers);
+	if (keys %autochangers == 0) {
+		printf "ERROR : There is no jukebox for this host in the datasource.\n";
+		exit 6;
+	} else {
+		# Check if there is no other CCCpivot running...
+		loadPidFile();
+		if (keys %processes != 0) {
+			printf "ERROR : There is still processes lauched in the PID file $pidPivotsFile.\n";
+			printf "ERROR : Can't start now. Stop, kill or purge processes before continue.\n";
+			exit 9;
+		} else {
+			open(my $pidFH, '>', $pidPivotsFile) or die "Could not open file '$pidPivotsFile' $!";
+			print $pidFH "key;pid\n";
+			# Launch one by one.
+			foreach my $key (sort(keys %autochangers)) {
+				my $jukebox = $autochangers{$key};
+				my %jukebox = %$jukebox;
+				# Set environment before launching the new process
+				$ENV{'CCCID'} = $jukebox{'id'};
+				$ENV{'CCCDESC'} = $jukebox{'desc'};
+				$ENV{'CCCWSSPORT'} = $jukebox{'tcpPort'};
+				$ENV{'CCCSERIAL'} = $jukebox{'serialPort'};
+				$ENV{'CCCBAUDS'} = $jukebox{'bauds'};
+				$ENV{'CCCDEBUG'} = (trim($cfgObj->param('logs.debug')) eq 'true') ? 1 : 0;
+				$ENV{'CCCTIMEOUT'} = $jukebox{'timeout'};
+				$ENV{'CCCPASS'} = $jukebox{'password'};
+				$ENV{'CCCMODEL'} = $jukebox{'model'};
+				$ENV{'CCCLPID'} = $jukebox{'leftPlayerID'};
+				if ((trim($jukebox{'useTLS'}) eq 'true')) {
+					$ENV{'CCCSSL'} = 1;
+					$ENV{'CCCSSLCERT'} = trim($cfgObj->param('ssl.certfile'));
+					$ENV{'CCCSSLDIR'} = trim($cfgObj->param('ssl.directory'));
+					$ENV{'CCCSSLKEY'} = trim($cfgObj->param('ssl.keyfile'));
+					$ENV{'CCCPASSPHR'} = trim($cfgObj->param('ssl.cccpivot'));
+				} 
+				printf("INFO : Starting CCCpivot script for %s - %s...",$jukebox{'id'}, $jukebox{'desc'});
+				# Start new Node.js process
+				my $proc = Proc::Simple->new();
+				my $logFile = $logDir.'cccpivot_'.$key.'.log';
+				$proc->redirect_output($logFile,$logFile);
+				$proc->start("node", $csvPivotScript,$jukebox{'id'},"> $logFile 2>&1");
+				my $pid = $proc->pid;
+				printf(" on PID n°%s.\n",$pid);
+				# Fill the PIDs CSV-like file, each row = a Node.js process
+				print $pidFH "$key;$pid\n";
+			}
+			close $pidFH;
+		}
+	}
+}
+
+# ------------------------ PROGRAM ------------------------ 
+if ($#ARGV + 1 != 1) {
+	printUsage();
+	exit 1;
+}
+switch ($ARGV[0]) {
+	case "start" {
+		printf "CCClauncher %s is starting...\n", SCRIPTVERSION;
+	}
+	case "restart" {
+		printf "CCClauncher %s is restarting...\n", SCRIPTVERSION;
+	}
+	case "status" {
+		printf "CCClauncher %s is displaying status...\n", SCRIPTVERSION;
+	}
+	case "stop" {
+		printf "CCClauncher %s is stopping...\n", SCRIPTVERSION;
+	}
+	case "clean" {
+		printf "CCClauncher %s is cleaning $pidPivotsFile...\n", SCRIPTVERSION;
+	}
+	case "kill" {
+		printf "CCClauncher %s is killing...\n", SCRIPTVERSION;
+	}
+	else {
+		printf "ERROR : Parameter %s unknown. Check input.\n", $ARGV[0];
+		printUsage();
+		exit 1
+	}
+}
+# Let's open the configuration file
+if (!-e $confFileName || !-f _ || !-r _ ) {
+	printf "ERROR : Configuration file %s not found or not readable.\n", $confFileName;
+	exit 2;
+}
+$cfgObj=new Config::Simple();
+if (!$cfgObj->read($confFileName)) {
+	printf "ERROR : Configuration file %s can't be opened by Config::Simple module.\n", $confFileName;
+	print $cfgObj->error();
+	exit 3;
+};
+# Check if some crucial parameters are not missing
+foreach (qw/general.datasource logs.debug logs.directory/) {
+	if (!trim($cfgObj->param("$_"))) {
+		printf "ERROR : The value of '%s' can't be empty in the configuration file %s.\n", $_, $confFileName;
+		exit 4;
+	}
+}
+# Do some actions according to the first parameter
+switch ($ARGV[0]) {
+	case "start" {
+		execStart();
+	}
+	case "status" {
+		# Get info from PIDs file and check processes
+		loadPidFile();
+		# Display informations
+		if (keys %processes == 0) {
+			printf "INFO : There is no running process in the PID file. Nothing to display.\n";
+			exit 10;
+		} else {
+			# Display informations
+			foreach my $key (sort(keys %processes)) {
+				my $process = $processes{$key};
+				my %process = %$process;
+				my $pid = $process{'pid'};
+				if (checkPIDExists($pid)) {
+					printf("INFO : CCCpivot script for %s : OK, process is running with PID n°%s.\n",$key,$pid);
+				} else {
+					printf("INFO : CCCpivot script for %s : KO, process is dead with PID n°%s.\n",$key,$pid);
+				}
+			}
+		}
+	}
+	case "stop" {
+		execStop();
+	}
+	case "restart" {
+		execStop();
+		execStart();
+	}
+	case "clean" {
+		system("rm $pidPivotsFile");
+		print "$pidPivotsFile removed.\n";
+	}
+	case "kill" {
+		execKill();
+	}
+}
+exit 0;
---
+++ readme.md
@@ -0,0 +1,164 @@
+# CCClauncher
+
+Introduction
+----------------------------------------------------------------
+[CCC = CAC Control Center](https://github.com/duprej/ccc). CAC is an accronym for dedicated Pioneer CD Autochangers.
+CCClauncher module is a friendly complement that helps you to use [CCCpivot](https://github.com/duprej/cccpivot) easily.
+
+It's basicaly a single Perl script to launch and manage all CCCpivot Node.js processes. This script reads a classical configuration file and needs a datasource.
+The datasource can be a CSV file or the CCCweb "autochanger" PostgreSQL table. The datasource describes all changers associated with their hostnames (of a computer).
+
+CCClauncher automaticaly scan the given datasource, filter it with the current hostname and startup all CCCpivot instances associated with this hostname.
+
+This allowing a central configuration of all autochangers shared by many computers accross a company (with file links / NFS).
+This Perl script can start, status and stop all your CCCpivot Node.js processes like a standard Linux deamon does (like via systemd).
+
+Files
+----------------------------------------------------------------
+
+| File | Description
+--- | ---
+| /opt/ccclauncher/ | Application directory
+| /opt/ccclauncher/launcher.pl | Perl script (managing Node.js processes)
+| /opt/ccclauncher/ccclauncher.cfg | Configuration (INI-like file)
+| /opt/ccclauncher/cccchangers.csv | CSV Datasource (local list of autochangers to manage)
+| /opt/ccclauncher/ccclauncher.service |  systemd unit
+
+Temporary Files
+----------------------------------------------------------------
+Managed process list is stored in a CSV pid file : /var/run/cccpivot.pids.
+```console
+root@dellpioneer:/opt/cccpivot# cat /var/run/cccpivot.pids
+key;pid
+jb1;2942
+jb2;2943
+```
+CCClauncher configuration
+----------------------------------------------------------------
+Once installed, edit the configuration file /etc/ccclauncher.cfg (INI file).
+Then edit the datasource file /etc/cccchangers.csv (CSV file).
+Each row of the CSV file is an autochanger entry :
+
+id;desc;enabled;serialPort;hostname;tcpPort;password;model;bauds;timeout;leftPlayerID
+
+| Entry | description
+--- | ---
+| id | Jukebox ID string (few chars), must be unique ! 
+| desc| Jukebox description (please avoid special chars).
+| enabled | true/false (false = autochanger ignored, no instance launched).
+| hostname | Computer hostname string (used to filter between many computers).
+| tcpPort | Port number for websocket listening (8000 and more).
+| password | Password string. Optional, leave it empty for easy operation.
+| model | Jukebox model string ['v180m','v3000','v3200','v5000'].
+| bauds | Serial connection speed.  Can be 4800/9600 on V3000/3200/5000 (check front switches). Must be set at 4800 for V180M (fixed on this model).
+| timeout | Serial timeout in seconds, maximum reaction time, advised values : 2 seconds for V3000/3200/5000. 12 seconds for V180M.
+| leftPlayerID | Left player ID number (for V3000/3200/5000 - see manual), put 0 for V180M.
+| useTLS | Enable TLS (use HTTPS certificate).
+
+Example :
+
+> id;desc;enabled;serialPort;hostname;tcpPort;password;model;bauds;timeout;leftPlayerID;useTLS
+> ac1;Changeur 1 V3000 (Gauche);true;/dev/ttyUSB1;dellpioneer;8000;;v3000;9600;2;1;true
+> ac2;Changeur 2 V3000 (Droite);true;/dev/ttyUSB2;dellpioneer;8001;;v3000;9600;2;1;true  
+> ac3;Changeur 3 V180M;true;/dev/ttyUSB0;dellpioneer;8002;;v180m;4800;12;0;true
+
+CCClauncher Perl script usage
+----------------------------------------------------------------
+```console
+root@dellpioneer:/opt/ccclauncher# perl launcher.pl toto
+ERROR : Parameter toto is unknown.
+This script needs a single parameter : [start|status|stop|restart|clean].
+Usage : launcher.pl [start|status|stop|restart|clean].
+```
+
+```console
+root@dellpioneer:/opt/ccclauncher# perl launcher.pl start
+CCClauncher 1.0 is starting...
+The hostname is dellpioneer.
+Datasource is CSV file.
+INFO : 2 jukeboxes loaded, 1 ignored.
+INFO : No PID file.
+INFO : Starting CCCpivot script for ac1 - Changeur 1 (Gauche)... on PID n°2942.
+INFO : Starting CCCpivot script for ac2 - Changeur 2 (Droite)... on PID n°2943.
+```
+
+```console
+root@dellpioneer:/opt/ccclauncher# perl launcher.pl status
+CCClauncher 1.0 is displaying status...
+INFO : CCCpivot script for ac1 : OK, process is running with PID n°2942.
+INFO : CCCpivot script for ac2 : OK, process is running with PID n°2943.
+```
+
+```console
+root@dellpioneer:/opt/ccclauncher# perl launcher.pl stop
+CCClauncher 1.0 is stopping...
+INFO : Stopping CCCpivot script for ac1... : OK, process n°2942 as been stopped properly.
+INFO : Stopping CCCpivot script for ac2... : OK, process n°2943 as been stopped properly.
+```
+
+Installation on Linux (in terminal)
+----------------------------------------------------------------
+All CCC modules are located in /opt directory by default.
+
+Check you have Perl & CPAN :
+
+```console
+perl -v
+cpan -v
+```
+
+Download the latest version to opt directory:
+
+```console
+sudo -- bash  -c 'cd /opt;git clone https://github.com/duprej/ccclauncher'
+```
+
+Check :
+
+```console
+sudo ls -R /opt/ccclauncher/
+```
+
+Install Perl modules :
+
+```console
+sudo cpan -i Config::Simple DBI Proc::Simple String::Util Sys::Hostname Text::CSV Switch
+```
+
+Copy the systemd unit file & enable at startup:
+
+```console
+sudo ln -s /opt/ccclauncher/ccclauncher.service ccclauncher.service
+sudo systemctl daemon-reload
+sudo systemctl enable ccclauncher
+```
+
+Install basic config files:
+
+```console
+sudo cp /opt/ccclauncher/conf/* /etc/
+```
+
+Configure/Personalize theses files:
+
+```console
+sudo vi /etc/ccclauncher.cfg
+sudo vi /etc/cccchangers.csv
+```
+
+Start your CCCpivot processes:
+
+```console
+sudo perl /opt/ccclauncher/launcher.pl start
+```
+
+Check if your CCCpivot processes are alive:
+
+```console
+ps -ef|grep node
+```
+```console
+sudo perl /opt/ccclauncher/launcher.pl status
+```
+
+Now you are ready to install or use tester, controller and web modules.
